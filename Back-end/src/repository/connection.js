8b26999e1ac2c89b4392fe28c7759c7693b67583
Denise Melo <nissemello@gmail.com>
2023-11-23 00:10:54 -0300

    commit deploid

--- Back-end/src/repository/connection.js
+++ Back-end/src/repository/connection.js
@@ -2,7 +2,7 @@ import mysql2 from 'mysql2/promise';
 
 
 const con = await mysql2.createConnection({
-    host: '4.201.80.48',
+    host: '191.234.194.31',
     database: 'dc',
     user: 'admin',
     password: '@dm!n',
